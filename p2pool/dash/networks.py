8f2c5821c6a69e1283f9d91fa30b3429746e0584
ivan <ivansib@users.noreply.github.com>
2017-02-06 13:18:08 +0100

    add sibcoin networks

--- p2pool/dash/networks.py
+++ p2pool/dash/networks.py
@@ -51,6 +51,56 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//2**32//1000000 - 1, 2**256//2**20 - 1),
         DUST_THRESHOLD=0.001e8,
     ),
+    sibcoin=math.Object(
+        P2P_PREFIX='bf0c6bbd'.decode('hex'),
+        P2P_PORT=1945,
+        ADDRESS_VERSION=63,
+        SCRIPT_ADDRESS_VERSION=40,
+        RPC_PORT=1944,
+        RPC_CHECK=defer.inlineCallbacks(lambda dashd: defer.returnValue(
+            'sibcoinaddress' in (yield dashd.rpc_help()) and
+            not (yield dashd.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda nBits, height: __import__('sib_subsidy').GetBlockBaseValue(nBits, height),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('x11_gost_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x11_gost_hash').getPoWHash(data)),
+        BLOCK_PERIOD=150,  # s
+        SYMBOL='SIB',
+        CONF_FILE_FUNC=lambda: os.path.join(
+            os.path.join(os.environ['APPDATA'], 'Sibcoin') if platform.system() == 'Windows' else os.path.expanduser(
+                '~/Library/Application Support/Sibcoin/') if platform.system() == 'Darwin' else os.path.expanduser(
+                '~/.sibcoin'), 'sibcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='https://chain.sibcoin.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='https://chain.sibcoin.net/address/',
+        TX_EXPLORER_URL_PREFIX='https://chain.sibcoin.net/tx/',
+        SANE_TARGET_RANGE=(2 ** 256 // 2 ** 32 // 1000000 - 1, 2 ** 256 // 2 ** 22 - 1),
+        DUST_THRESHOLD=0.001e8,
+    ),
+    sibcoin_testnet=math.Object(
+        P2P_PREFIX='cee2caff'.decode('hex'),
+        P2P_PORT=11945,
+        ADDRESS_VERSION=125,
+        SCRIPT_ADDRESS_VERSION=100,
+        RPC_PORT=11944,
+        RPC_CHECK=defer.inlineCallbacks(lambda dashd: defer.returnValue(
+            'sibcoinaddress' in (yield dashd.rpc_help()) and
+            (yield dashd.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda nBits, height: __import__('sib_subsidy').GetBlockBaseValue_testnet(nBits, height),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('x11_gost_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('x11_gost_hash').getPoWHash(data)),
+        BLOCK_PERIOD=150,  # s
+        SYMBOL='tSIB',
+        CONF_FILE_FUNC=lambda: os.path.join(
+            os.path.join(os.environ['APPDATA'], 'Sibcoin') if platform.system() == 'Windows' else os.path.expanduser(
+                '~/Library/Application Support/Sibcoin/') if platform.system() == 'Darwin' else os.path.expanduser(
+                '~/.sibcoin'), 'sibcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='https://testchain.sibcoin.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='https://testchain.sibcoin.net/address/',
+        TX_EXPLORER_URL_PREFIX='https://testchain.sibcoin.net/tx/',
+        SANE_TARGET_RANGE=(2 ** 256 // 2 ** 32 // 1000000 - 1, 2 ** 256 // 2 ** 20 - 1),
+        DUST_THRESHOLD=0.001e8,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -46,6 +46,46 @@ nets = dict(
         ANNOUNCE_CHANNEL='',
         VERSION_CHECK=lambda v: True,
     ),
+    sibcoin=math.Object(
+        PARENT=networks.nets['sibcoin'],
+        SHARE_PERIOD=20,  # seconds
+        CHAIN_LENGTH=24 * 60 * 60 // 20,  # shares
+        REAL_CHAIN_LENGTH=24 * 60 * 60 // 20,  # shares
+        TARGET_LOOKBEHIND=100,
+        # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=10,  # blocks
+        IDENTIFIER='02d39200fb47ae70'.decode('hex'),
+        PREFIX='8a35547e875f3f5f'.decode('hex'),
+        COINBASEEXT='0D2F5032506F6F6C2D444153482F'.decode('hex'),
+        P2P_PORT=8945,
+        MIN_TARGET=0,
+        MAX_TARGET=2 ** 256 // 2 ** 20 - 1,
+        PERSIST=True,
+        WORKER_PORT=7903,
+        BOOTSTRAP_ADDRS='p2pool.sibcoin.net'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-sib',
+        VERSION_CHECK=lambda v: v >= 110217,
+    ),
+    sibcoin_testnet=math.Object(
+        PARENT=networks.nets['sibcoin_testnet'],
+        SHARE_PERIOD=20,  # seconds
+        CHAIN_LENGTH=24 * 60 * 60 // 20,  # shares
+        REAL_CHAIN_LENGTH=24 * 60 * 60 // 20,  # shares
+        TARGET_LOOKBEHIND=100,
+        # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=10,  # blocks
+        IDENTIFIER='c72af0f5c34a810b'.decode('hex'),
+        PREFIX='41161f7eb6ceaf73'.decode('hex'),
+        COINBASEEXT='0E2F5032506F6F6C2D74444153482F'.decode('hex'),
+        P2P_PORT=18945,
+        MIN_TARGET=0,
+        MAX_TARGET=2 ** 256 // 2 ** 20 - 1,
+        PERSIST=False,
+        WORKER_PORT=17903,
+        BOOTSTRAP_ADDRS='p2pool.sibcoin.net'.split(' '),
+        ANNOUNCE_CHANNEL='',
+        VERSION_CHECK=lambda v: True,
+    ),
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
