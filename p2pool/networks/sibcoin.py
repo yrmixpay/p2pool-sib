7a7479314b2b05b5f8ff4448165f4e280ac5cbac
ivan <ivansib@users.noreply.github.com>
2017-04-25 02:33:00 +0200

    add miningclub

--- p2pool/networks/sibcoin.py
+++ p2pool/networks/sibcoin.py
@@ -14,6 +14,6 @@ MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = True
 WORKER_PORT = 9345
-BOOTSTRAP_ADDRS = 'p2pool.sibcoin.net p2pool.darknode.ru crabs.pro'.split(' ')
+BOOTSTRAP_ADDRS = 'p2pool.sibcoin.net p2pool.darknode.ru crabs.pro sib.miningclub.info'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-sib'
 VERSION_CHECK = lambda v: v >= 160100
